fe4f6bb2ce775929eacfea246640b53bca61dbcb
= <renardhj@gmail.com>
2017-11-18 17:01:27 -0500

    File Reading

--- scripts/script.js
+++ scripts/script.js
@@ -3,61 +3,68 @@ let imageDownloadable = false;
 let file;
 let drawMethod;
 
-$( document ).ready(function() {
+$(document).ready(function() {
   console.log("Document Loaded.");
 });
 
 // check if a correct file type is selected
-$("#fileinput").change(function()
-{
-    if(this.value.substr(this.value.length - 3) != ".py" && this.value.substr(this.value.length - 3) != ".json")
-    {
-        isValid = false;
-        alert("Not a .py file!");
-    }
-    else
-    {
-      isValid = true;
+$("#fileinput").change(function() {
+  if (this.value.substr(this.value.length - 3) != ".py" && this.value.substr(this.value.length - 3) != ".json") {
+    isValid = false;
+    alert("Not a .py file!");
+  } else {
+    isValid = true;
 
     file = this.value;
     fileName = file.split('.')[0];
     fileName = fileName.split("C:\\fakepath\\")[1];
     console.log(fileName);
-  }
+
+    file = this.files[0];
+
+    var reader = new FileReader();
+    reader.onload = function(progressEvent) {
+      // Entire file
+      console.log(this.result);
+
+      // By lines
+      var lines = this.result.split('\n');
+      for (var line = 0; line < lines.length; line++) {
+        console.log(lines[line]);
+      }
+    }
+  };
+  reader.readAsText(file);
 });
 
 // navigate the tab
-$(".nav a").on("click", function()
-{
-    $(".nav .active").removeClass("active");
-    $(this).parent().addClass("active");
+$(".nav a").on("click", function() {
+  $(".nav .active").removeClass("active");
+  $(this).parent().addClass("active");
 
-    drawMethod = $(this).parent().attr('id');
-    console.log(drawMethod);
+  drawMethod = $(this).parent().attr('id');
+  console.log(drawMethod);
 
-    return false;
+  return false;
 });
 
 // If create image button is clicked
-$(".createPic").on("click", function()
-{
-    if(!isValid) alert("No .py or .json file!");
-    else
-    {
-      switch(drawMethod)
-      {
-        case "drawMethod1":
-          // code block
-          console.log("temp1");
-          imageDownloadable = true;
-          break;
-        case "drawMethod2":
-          // code block
-          console.log("temp2");
-          imageDownloadable = true;
-          break;
-        }
+$(".createPic").on("click", function() {
+  if (!isValid) alert("No .py or .json file!");
+  else {
+    switch (drawMethod) {
+      case "drawMethod1":
+        // code block
+        console.log("temp1");
+        imageDownloadable = true;
+        break;
+      case "drawMethod2":
+        // code block
+        console.log("temp2");
+        imageDownloadable = true;
+        break;
     }
+  }
 
-    return false;
+  return false;
 });
