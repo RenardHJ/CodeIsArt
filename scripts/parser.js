47493c456b4f6ff1595e45a253441889a92e72d8
Adyke95 <aaronlee9195@gmail.com>
2017-11-18 19:32:24 -0500

    Moved the parser to parser.js

---
+++ scripts/parser.js
@@ -0,0 +1,52 @@
+var reader = new FileReader();
+reader.onload = function (progressEvent) {
+    // Entire file
+    console.log(this.result);
+
+    var outputJson = {
+        "type": "program",
+        "name": fileName,
+        "body": []
+    };
+    // By lines
+    var lines = this.result.split('\n');
+    for (var line = 0; line < lines.length; line++) {
+        // TODO: parse line
+        var depth = lines[line].search(/\S|$/) % 4;
+        console.log(depth);
+        console.log(lines[line]);
+        if (lines[line].match(pythonRegexDict["class"])) {
+            // define a class
+            console.log("class");
+        }
+        else if (lines[line].match(pythonRegexDict["function"])) {
+            // define a function
+            console.log("fucntion");
+        }
+        else if (lines[line].match(pythonRegexDict["for"])) {
+            // for loop
+            console.log("for");
+        }
+        else if (lines[line].match(pythonRegexDict["while"])) {
+            // while loop
+            console.log("while");
+        }
+        else if (lines[line].match(pythonRegexDict["else"])) {
+            // else statement
+            console.log("else");
+        }
+        else if (lines[line].match(pythonRegexDict["else if"])) {
+            // else if statement
+            console.log("else if");
+        }
+        else if (lines[line].match(pythonRegexDict["if"])) {
+            // if statement
+            console.log("if");
+        }
+        else {
+            // non foldable line
+            console.log("Non-foldable");
+        }
+
+    }
+}
--- scripts/script.js
+++ scripts/script.js
@@ -33,58 +33,7 @@ $("#fileinput").change(function() {
     file = this.files[0];
 
     if (this.value.substr(this.value.length - 3) == ".py"){
-      var reader = new FileReader();
-      reader.onload = function(progressEvent) {
-        // Entire file
-        console.log(this.result);
-
-        var outputJson = {
-          "type": "program",
-          "name": fileName,
-          "body": []
-        };
-        // By lines
-        var lines = this.result.split('\n');
-        for (var line = 0; line < lines.length; line++) {
-          // TODO: parse line
-          var depth = lines[line].search(/\S|$/) % 4;
-          console.log(depth);
-          console.log(lines[line]);
-          if(lines[line].match(pythonRegexDict["class"])) {
-            // define a class
-            console.log("class");
-          }
-          else if (lines[line].match(pythonRegexDict["function"])){
-            // define a function
-            console.log("fucntion");
-          }
-          else if (lines[line].match(pythonRegexDict["for"])) {
-            // for loop
-            console.log("for");
-          }
-          else if (lines[line].match(pythonRegexDict["while"])){
-            // while loop
-            console.log("while");
-          }
-          else if (lines[line].match(pythonRegexDict["else"])) {
-            // else statement
-            console.log("else");
-          }
-          else if (lines[line].match(pythonRegexDict["else if"])){
-            // else if statement
-            console.log("else if");
-          }
-          else if (lines[line].match(pythonRegexDict["if"])){
-            // if statement
-            console.log("if");
-          }
-          else{
-            // non foldable line
-            console.log("Non-foldable");
-          }
-
-        }
-      }
+      // parser.js
     }
   };
   reader.readAsText(file);
