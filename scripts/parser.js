bec9acd12ee9f338c1fa521647d14d9e835a3c67
Hunter Renard <renardhj@gmail.com>
2017-11-20 16:27:40 -0500

    Update parser.js

--- scripts/parser.js
+++ scripts/parser.js
@@ -8,16 +8,16 @@ function depthStringGenerator(depthStack){
 
 function parserFunction(lines){
     var pythonRegexDict = {
-        "class": "\s+class .+:",
-        "function": "\s+def .+:",
-        "for": "\s+for .+:",
-        "while": "\s+while .+:",
-        "if": "\s+if .+:",
-        "else if": "\s+elif .+:",
-        "else": "\s+else:",
-        "try" : "\s+try:",
-        "except" : "\s+except .*:",
-        "comment": "\s+#"
+        "class": "^\s*class .+:",
+        "function": "^\s*def .+:",
+        "for": "^\s*for .+:",
+        "while": "^\s*while .+:",
+        "if": "^\s*if .+:",
+        "else if": "^\s*elif .+:",
+        "else": "^\s*else:",
+        "try" : "^\s*try:",
+        "except" : "^\s*except .*:",
+        "comment": "^\s*#"
       };
 
     var outputJson = {
