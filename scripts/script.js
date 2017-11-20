7da6195acaf5176715878643d14cdb246c993508
hrenard <hrenard@mail.umw.edu>
2017-11-20 12:11:51 -0500

    Adding Clear Canvas Functionality

--- scripts/script.js
+++ scripts/script.js
@@ -77,3 +77,11 @@ $(".createPic").on("click", function()
 
   return false;
 });
+
+$(".clearCanvas").on("click", function()
+{
+    var canvas = document.getElementById("drawingCanvas");
+    var context = canvas.getContext("2d"); 
+    context.clearRect(0, 0, canvas.width, canvas.height);
+});
+
