5b7aac7e4aa8d1ba6caadbecbc318c7ca69c8773
Hunter Renard <renardhj@gmail.com>
2017-11-21 18:29:12 -0500

    changing canvas size

--- scripts/sketch.js
+++ scripts/sketch.js
@@ -3,13 +3,13 @@ var canvasHeight;
 var canvasWidth;
 
 function setup() {
-  var w = document.getElementById('canvas_container').offsetWidth / 2;
-  var h = w;
+  var w = document.getElementById('canvas_container').offsetWidth;
+  var h = w / 2;
 
   canvas = createCanvas(w, h);
   noFill();
   stroke("lightgray");
-  rect(0, 0, w - 1, w - 1);
+  rect(0, 0, w - 1, h - 1);
   stroke("black");
 
   // Move the canvas so it's inside our <div id="sketch-holder">.
